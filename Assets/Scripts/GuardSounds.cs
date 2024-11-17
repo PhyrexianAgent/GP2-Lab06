dd1b884cbaefad99cb6ca7789c05c39e6d5577a2
PhyrexianAgent <aidanthewizard5@gmail.com>
2024-11-17 14:20:15 -0500

    Plays Footstep Sound Every Step
    I used an asset I looked up for this.

--- Assets/Scripts/GuardSounds.cs
+++ Assets/Scripts/GuardSounds.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class GuardSounds : MonoBehaviour
 {
+    
+    [SerializeField] private AudioClip stepSound;
+
+    private AudioSource audio;
+    void Awake(){
+        audio = GetComponent<AudioSource>();
+    }
     public void PlayStep(){
-        Debug.Log("step");
+        audio.PlayOneShot(stepSound);
     }
 }
